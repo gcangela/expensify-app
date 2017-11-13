f057d8ac9fcba4ba8fb16816e30adf8e0a71cbf1
gcangela <gcangela90@gmail.com>
2017-11-13 16:17:54 +0100

    made edit link dynamic

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const EditExpensePage = () => (
+const EditExpensePage = (props) => {
+  console.log(props)
+ return (
   <div>
-   This is from my edit page component.
- </div>
+    Editing the expense with id of {props.match.params.id}
+  </div>
  )
+}
 
  export default EditExpensePage
