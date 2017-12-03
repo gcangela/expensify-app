92c35fb7dd314c649d435d8aee2af8e9af24cee2
gcangela <gcangela90@gmail.com>
2017-12-03 14:20:11 +0100

    added expense summary to the dashboard

---
+++ src/tests/selectors/expenses-total.test.js
@@ -0,0 +1,16 @@
+import selectExpensesTotal from '../../selectors/expenses-total'
+import expenses from  '../fixtures/expenses'
+
+test('should return 0 if no expenses', () => {
+  const result = selectExpensesTotal([])
+  expect(result).toBe(0)
+})
+
+test('should add up a single expense', () => {
+  const result = selectExpensesTotal([expenses[0]])
+  expect(result).toBe(195)
+})
+test('should add up a single expense', () => {
+  const result = selectExpensesTotal(expenses)
+  expect(result).toBe(114195)
+})
